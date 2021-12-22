5e38ddd6404dc54e4b0cc7b894633600bfee68d7
eresha1231 <eresha1231@gmail.com>
2021-12-22 20:52:54 +0700

    2-module/3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,18 @@
 let calculator = {
-  // ваш код
+  num1: 0,
+  num2: 0,
+  read(arg1, arg2) {
+    this.num1 = arg1;
+    this.num2 = arg2;
+  },
+  sum() {
+    return this.num1 + this.num2;
+  },
+  mul() {
+    return this.num1 * this.num2;
+  }
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
