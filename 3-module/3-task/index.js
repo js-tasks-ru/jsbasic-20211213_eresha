933fc815b3d3292c696c68cdfb97c692b14f2380
eresha1231 <eresha1231@gmail.com>
2022-01-02 22:23:45 +0700

    4-module/1-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,13 +1,17 @@
 function camelize(str) {
-  let arrSymbols = str.split('');
-  let newArr = [];
-  for (let i = 0; i < arrSymbols.length; i++) {
-    if (arrSymbols[i] === '-') {
-      newArr.push(arrSymbols[i + 1].toUpperCase());
-      i++;
-    } else {
-      newArr.push(arrSymbols[i]);
-    }
-  }
-  return newArr.join('');
+  return str.split('-').map((elem, index) => (index === 0) ? elem : elem[0].toUpperCase() + elem.slice(1)).join('');
 }
+
+// function camelize(str) {
+//   let arrSymbols = str.split('');
+//   let newArr = [];
+//   for (let i = 0; i < arrSymbols.length; i++) {
+//     if (arrSymbols[i] === '-') {
+//       newArr.push(arrSymbols[i + 1].toUpperCase());
+//       i++;
+//     } else {
+//       newArr.push(arrSymbols[i]);
+//     }
+//   }
+//   return newArr.join('');
+// }
--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement('ul');
+  let li = friends.map(friend => {
+    return `<li>${friend.firstName} ${friend.lastName}</li>`;
+  }).join(`\n`);
+  ul.insertAdjacentHTML(`beforeend`, li);
+  return ul;
 }
