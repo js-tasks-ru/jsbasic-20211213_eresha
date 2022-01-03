d48c761499574223b060c6628f6788a5574daca0
eresha1231 <eresha1231@gmail.com>
2022-01-03 17:12:27 +0700

    4-module/3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+  let tBodyes = table.tBodies;
+  for (let tBodye of tBodyes) {
+    for (let row of tBodye.rows) {
+      if (+row.cells[1].textContent < 18) {
+        row.style = "text-decoration: line-through";
+      }
+      if (row.cells[2].textContent === `m`) {
+        row.classList.add('male');
+      }
+      if (row.cells[2].textContent === `f`) {
+        row.classList.add('female');
+      }
+      if (row.cells[3].dataset.available === 'true') {
+        row.classList.add('available');
+      }
+      if (row.cells[3].dataset.available === 'false') {
+        row.classList.add('unavailable');
+      }
+      if (row.cells[3].dataset.available === undefined) {
+        row.setAttribute('hidden', true);
+      }
+    }
+  }
 }
