ba3bb6e209e4f32f0993004bbddb6a2e4fdb5827
eresha1231 <eresha1231@gmail.com>
2022-01-07 14:40:19 +0700

    5-module/3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,5 +1,7 @@
 function initCarousel() {
   const carousel = document.querySelector(`.carousel`);
+
+  // const carouselArrow = carousel.querySelector(`.carousel__arrow`);
   const arrowRight = carousel.querySelector(`.carousel__arrow_right`);
   const arrowLeft = carousel.querySelector(`.carousel__arrow_left`);
   const carouselInner = carousel.querySelector(`.carousel__inner`);
@@ -11,12 +13,12 @@ function initCarousel() {
 
   const moveSlide = (n) => {
     carouselInner.style.transform = `translateX(-${widthSlide * n}px)`;
-    arrowLeft.style.display = arrowRight.style.display = 'none';
-    if (n !== 0) {
-      arrowLeft.style.display = '';
+    arrowLeft.style.display = arrowRight.style.display = '';
+    if (n === 0) {
+      arrowLeft.style.display = 'none';
     }
-    if (n !== carouselSlides.length - 1) {
-      arrowRight.style.display = '';
+    if (n === carouselSlides.length - 1) {
+      arrowRight.style.display = 'none';
     }
   };
 
