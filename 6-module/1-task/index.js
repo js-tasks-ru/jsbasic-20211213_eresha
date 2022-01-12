2ba18ec07d5a19ac566059102b40c9448542f1f6
eresha1231 <eresha1231@gmail.com>
2022-01-12 22:09:38 +0700

    6-module-1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,50 @@
  *      }
  *
  */
+
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+    this._deleteBtn();
+  }
+
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `<thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    ${this._renderCells()}
+    </tbody>`;
+  }
+
+  _renderCells() {
+    return this.rows.map((row) => {
+      return `<tr>
+      <td>${row.name}</td>
+      <td>${row.age}</td>
+      <td>${row.salary}</td>
+      <td>${row.city}</td>
+      <td><button>X</button></td>
+    </tr>`;
+    });
+  }
+
+  _deleteBtn() {
+    const buttons = this.elem.querySelectorAll('button');
+    buttons.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        btn.closest('tr').remove();
+      });
+    });
   }
 }
+
